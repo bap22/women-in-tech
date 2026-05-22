5186d8ac7c2c10cc7eca92e7c10e9b039cd36291
Brett Peterson <2632861+bap22@users.noreply.github.com>
2026-05-22 10:36:56 -0600

    Fix urlFor to return null when client/source missing

--- sanity/lib/image.ts
+++ sanity/lib/image.ts
@@ -2,7 +2,7 @@ import createImageUrlBuilder from '@sanity/image-url'
 import { client } from './client'
 
 export const urlFor = (source: any) => {
-  if (!client) return ''
+  if (!client || !source) return null
   const builder = createImageUrlBuilder(client)
   return builder.image(source)
 }
